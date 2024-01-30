f9043595c3468c335f260df3ec0d3180f3733fba
Oxana Kolbasina <topnoga@mail.ru>
2024-01-30 19:40:14 +0300

    Исправления тестов

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,7 @@
-from src.utils import load_operations
+import pytest
+from models.operation import Operation
+
+from utils import load_operations, get_operation_instances
 
 
 def test_load_operations():
@@ -8,8 +11,8 @@ def test_load_operations():
     assert operations[0]["id"] == 441945886
 
     path = "sdfghhjjk,gfghjkkk"
-    with pytest. raises(FileExistsError):
-        test_load_operations(path)
+    with pytest.raises(FileNotFoundError):
+        load_operations(path)
 
 
 def test_get_opertion_instances():
@@ -30,18 +33,39 @@ def test_get_opertion_instances():
             "to": "Счет 64686473678894779589"
     }
         ]
-    operation_instances = get_opertion_instances(test_operations)
+    operation_instances = get_operation_instances(test_operations)
     assert isinstance(operation_instances, list)
     assert isinstance(operation_instances[0], Operation)
     assert operation_instances[0].pk == 441945886
 
     test_operations =[]
-    operation_instances = get_opertion_instances(test_operations)
+    operation_instances = get_operation_instances(test_operations)
     assert operation_instances == []
 
     test_operations = [{}]
     operation_instances = get_operation_instances(test_operations)
     assert operation_instances == []
+
+
+def test_get_operations():
+    operation = Operation(
+        pk=441945886,
+        state="EXECUTED",
+        date="2019-08-26T10:50:58.294041",
+        operation_amount={
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        description="Перевод организации",
+        _from="Maestro 1596837868705199",
+        _to="Счет 64686473678894779589"
+    )
+    assert operation.date == "26.08.2019"
+    assert operation._from == "Maestro 1596 83** **** 5199"
+    assert operation._to == "Счет ** 9589"
 
 
 
